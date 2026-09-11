424a8bbe87be351174318f323107dad3df896bf1
Mehjab U <mehjabuddin@hotmail.com>
2026-09-10 22:47:47 -0400

    - avg water intensity by city added

--- database/queries.sql
+++ database/queries.sql
@@ -11,3 +11,10 @@ FROM buildings b
 JOIN energy_performance e ON b.ewrb_id = e.ewrb_id
 ORDER BY e.ghg_intensity_kgco2e_m2 DESC
 LIMIT 10;
+
+SELECT b.city,
+       ROUND(AVG(e.water_intensity_m3_m2)::numeric, 2) AS avg_water_intensity
+FROM buildings b
+JOIN energy_performance e ON b.ewrb_id = e.ewrb_id
+GROUP BY b.city
+ORDER BY avg_water_intensity DESC;
