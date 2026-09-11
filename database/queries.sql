4f0993650ca64dc3380fd8f1bf649f001abb8587
Mehjab U <mehjabuddin@hotmail.com>
2026-09-10 22:45:39 -0400

    - moved onto the next attributes (in relation to previous queries.sql changes)

--- database/queries.sql
+++ database/queries.sql
@@ -5,3 +5,9 @@ FROM buildings b
 JOIN energy_performance e ON b.ewrb_id = e.ewrb_id
 GROUP BY b.primary_property_type
 ORDER BY avg_site_eui DESC;
+
+SELECT b.ewrb_id, b.city, b.primary_property_type, e.ghg_intensity_kgco2e_m2
+FROM buildings b
+JOIN energy_performance e ON b.ewrb_id = e.ewrb_id
+ORDER BY e.ghg_intensity_kgco2e_m2 DESC
+LIMIT 10;
